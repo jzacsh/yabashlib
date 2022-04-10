34de5385b8b6bdab114aae247a7af111555a9c6b
Jonathan Zacsh <j@zac.sh>
2022-04-10 16:29:55 -0500

    new prompt confirmation wrapper to simplify interactive code

--- src/console.sh
+++ src/console.sh
@@ -7,3 +7,42 @@ function term_supports_color() {
   local tput_out; tput_out="$(tput colors 2>/dev/null)"
   (( $? == 0 )) && [[ "$tput_out" -gt 2 ]]
 }
+
+# Base internal logic used by term_confirm.
+#
+# $1=requires 'optin' or 'optout' to indicate what is required for prompt to
+# succeed.
+# $2=prompt
+function __yabashlib__term_confirm() {
+  local answer
+  if [[ "$1" = optout ]]; then
+    # This case: special-casing is the requirement that a user opt-out if they
+    # don't want to proceed in the positive, otherwise we will proceed.
+    read -p "$2 [Y/n] " answer
+
+    if strIsEmptyish "$answer"; then
+      return 0 # proceed
+    fi
+  else
+    # This case: default is to require opt in if users want to proceed in the
+    # positive, otherwise we'll not proceed.
+
+    read -p "$2 [y/N] " answer
+    if strIsEmptyish "$answer"; then
+      return 1 # do not proceed
+    fi
+  fi
+  [[ "${answer,,}" = y || "${answer,,}" = yes ]]
+}
+
+function term_confirm_optin() {
+  __yabashlib__term_confirm optin "$@"
+}
+
+function term_confirm_optout() {
+  __yabashlib__term_confirm optout "$@"
+}
+
+function term_confirm() {
+  term_confirm_optin "$@"
+}
