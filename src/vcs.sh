dfce3c0602aea40bf319514995db20db2d83743e
Jonathan Zacsh <j@zac.sh>
2022-04-10 16:02:56 -0500

    vcs feature: allow for dir as arg

--- src/vcs.sh
+++ src/vcs.sh
@@ -3,7 +3,19 @@
 # Utilities helpful when dealing with version control systems (VCS).
 
 # TODO add test coverage
-function vcs_git_is_clean() { [[ -z "$(git status --porcelain)" ]]; }
+function vcs_git_is_clean() (
+  [[ "$#" -eq 0 ]] || {
+    cd "$1" || logfFatal \
+      'cannot inspect git status; failed to CD to\n\t"%s"\n' "$1"
+    }
+  [[ -z "$(git status --porcelain)" ]];
+)
 
 # TODO add test coverage
-function vcs_git_is_dirty() { ! vcs_git_is_clean; }
+function vcs_git_is_dirty() (
+  [[ "$#" -eq 0 ]] || {
+    cd "$1" || logfFatal \
+      'cannot inspect git status; failed to CD to\n\t"%s"\n' "$1"
+  }
+  ! vcs_git_is_clean "$@"
+)
