59849462dbd8589d625186691f329f33b486fd56
Jonathan Zacsh <j@zac.sh>
2022-11-09 17:38:59 -0600

    start vcs port code from my own dotfiles and rename/namespace it

--- src/vcs.sh
+++ src/vcs.sh
@@ -20,3 +20,36 @@ function vcs_git_is_dirty() (
   }
   ! vcs_git_is_clean "$@"
 )
+
+# TODO finish porting (and deleting originals!) code originally from vcs_aliases.sh into yabashlib
+# originals live here: https://gitlab.com/jzacsh/dotfiles/-/blob/0925fee64e1/bin/lib/vcs_aliases.sh
+function __yblib.vcsGit_root() { git rev-parse --show-toplevel; }
+function __yblib.vcsHg_root() { hg root; }
+function __yblib.vcsIs_in_hg() {
+  if __yblib.vcsis_in_git "$1"; then return 1; fi
+  ( cd "$1"; hg root >/dev/null 2>&1; )
+}
+
+function __yblib.vcsis_in_git(){
+  ( cd "$1"; git rev-parse --show-toplevel >/dev/null 2>&1; )
+}
+
+# Prints and `cd`s into root dir.
+function yblib.vcsRoot() {
+  local root_dir="$(
+    if __yblib.vcsis_in_git; then
+      __yblib.vcsGit_root
+    elif __yblib.vcsIs_in_hg; then
+      __yblib.vcsHg_root
+    else
+      printf '$PWD not a known VCS repo\n' >&2
+      return 1
+    fi
+  )"
+
+  # Print in case somene uses this function in a subshell, for an assignment
+  echo "$root_dir"
+
+  # CD in case someone uses this function interactively
+  cd "$root_dir"
+}
