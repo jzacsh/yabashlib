8d801cff0ec17ef7f593462c49ae71638fd4c5e6
Jonathan Zacsh <j@zac.sh>
2024-11-10 12:00:21 -0600

    new api: share extant `scrap_dir` from dotfiles
    I very frequently (at least daily) use a [`scrap_dir` alias][today]
    along with a slug parameter to quickly jump to new locations/files to
    jot down notes/text snippets, or do little experiments, without worrying
    that I'm muddying up downloads folder with monotonic growth. Knowing my
    `scraps_dir` content goes somewhere together lets me easily
    analyze/discard/ignore it later if I want. It also ensures I don't have
    a ton of `foo` or `test.txt` files all over my $HOME or downloads folder
    when I just quickly needed to take a quick note or run a 3 min
    experiment (and so couldn't be bothered by the distraction of having to
    find a location and a name for such a fleeting act, just to ensure I
    don't pay the complexity cost later of wondering what all these strange
    files are and if they're mid-progress (and so shouldn't be deleted)).
    
    This is apparently time-tested: it seems I [started doing this a decade
    ago][origin].
    
    [today]: https://gitlab.com/jzacsh/dotfiles/-/blob/abad72673026ebc4d8176f3765e457327bb8aee3/.bash_aliases#L589-630
    [origin]: https://gitlab.com/jzacsh/dotfiles/-/commit/7c1d4cde43458374bb6e4a12728edda5054b3be1

---
+++ src/mktemp.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+#
+# Make human-readable temporary files; ie: mktemp, but for humans.
+
+# state that needs reset for every caller (who will source us) should be above
+# this guard.
+[[ -z "${_yblib_guard_mktemp:-}" ]] || return 0; _yblib_guard_mktemp=1 # include guard
+
+# TODO(https://github.com/bats-core/bats-core/issues/509) figure out the latest
+# way to test while test-doubling bash builtins, then write tests for this file.
+
+function __yblib.mkHumanTempTemplatePrefix() {
+  date '+%Y%m%d-at-%s'
+}
+
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function __yblib.mkHumanTempTemplate() {
+  local slug="$1" sortablePrefix
+
+  sortablePrefix="$(__yblib.mkHumanTempTemplatePrefix)" || return 1
+  printf -- \
+    '%s_%s_XXXXXXXX' \
+    "$sortablePrefix" \
+    "$slug"
+}
+
+# `mktemp` with a default template that's human-readable, sortable, and whose
+# age is immediately obvious at a glance.
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+#
+# internal details: see __yblib.mkHumanTempTemplate
+function yblib.mkHumanTemp() {
+  local slug="$1" templ
+  [[ "$#" -eq 1 ]] || {
+    logfError 'usage: require SLUG as only arg\n'
+    return 1
+  }
+
+  templ="$(__yblib.mkHumanTempTemplate "$slug")" || return 1
+  mktemp --tmpdir=  "$templ"
+}
+
+# like yblib.mkHumanTemp but in $PWD
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function yblib.mkHumanLocalTemp() { TMPDIR=. yblib.mkHumanTemp "$@"; }
+
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function __yblib.mkHumanTempTemplateDir() {
+  local slug="$1" sortablePrefix
+
+  sortablePrefix="$(__yblib.mkHumanTempTemplatePrefix)" || return 1
+  printf -- \
+    '%s_%s_XXXXXXXX.d' \
+    "$sortablePrefix" \
+    "$slug"
+}
+
+# Annoying version of __yblib.mkHumanTempTemplateDir just for OSX (and maybe
+# BSD?) users whose `mktemp` _requires_ XXXXXXX to be the last chunk of a
+# template.
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function __yblib.mkHumanTempTemplateDirPortable() {
+  local slug="$1" sortablePrefix
+
+  sortablePrefix="$(__yblib.mkHumanTempTemplatePrefix)" || return 1
+  printf -- \
+    '%s_%s_d-XXXXXXXX' \
+    "$sortablePrefix" \
+    "$slug"
+}
+
+# `mktemp -d` with a default template that's human-readable, sortable, and whose
+# age is immediately obvious at a glance.
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+#
+# internal details: see __yblib.mkHumanTempTemplateDir
+function yblib.mkHumanTempDir() {
+  local slug="$1" templ
+  [[ "$#" -eq 1 ]] || {
+    logfError 'usage: require SLUG as only arg\n'
+    return 1
+  }
+
+  templ="$(__yblib.mkHumanTempTemplateDir "$slug")" || return 1
+  mktemp --tmpdir=  --directory "$templ"
+}
+
+# More portable version of yblib.mkHumanTempDir
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function yblib.mkHumanTempDirSafe() {
+  local slug="$1" templ
+  [[ "$#" -eq 1 ]] || {
+    logfError 'usage: require SLUG as only arg\n'
+    return 1
+  }
+
+  templ="$(__yblib.mkHumanTempTemplateDirPortable "$slug")" || return 1
+  mktemp --tmpdir=  --directory "$templ"
+}
+
+
+
+# like yblib.mkHumanTempDir but in $PWD
+#
+# $1=brief human-written slug to conceptually remind what this temp was about.
+function yblib.mkHumanLocalTempDir() { TMPDIR=. yblib.mkHumanTempDir "$@"; }
